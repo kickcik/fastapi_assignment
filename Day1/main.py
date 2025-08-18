67c993529c470f9660e583574885676d6149d8d5
kickcik <4778009@gmail.com>
2025-08-18 19:11:55 +0900

    Days 폴더 구분

--- Day1/main.py
+++ Day1/main.py
@@ -94,4 +94,4 @@ def search_users(params: Annotated[UserSearchParams, Query()]):
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=8000)
