b9460707c13f654a13783bc4e9d9ffcbf13719ea
kickcik <4778009@gmail.com>
2025-08-19 16:11:59 +0900

    스키마 적용

--- Day1/main.py
+++ Day1/main.py
@@ -1,42 +1,11 @@
 # main.py
 
-from enum import Enum
 from typing import Annotated
 
 from fastapi import FastAPI, HTTPException, Path, Query
-from pydantic import BaseModel
+
+from Day1.app.schemas.users import UserCreateSchema, UserUpdateSchema, UserSearchParams, UserResponse
 from app.models.users import UserModel
-
-class GenderName(str, Enum):
-    male = "male"
-    female = "female"
-
-class UserCreateSchema(BaseModel):
-    username: str
-    age: int
-    gender: GenderName
-
-class UserUpdateSchema(BaseModel):
-    username: str | None = None
-    age: int | None = None
-    gender: GenderName | None = None
-
-class UserSearchParams(BaseModel):
-    model_config = {'extra':'forbid'}
-
-    username: str | None = None
-    age: int | None = None
-    gender: GenderName | None = None
-
-class UserResponse(BaseModel):
-    id: int
-    username: str
-    age: int
-    gender: GenderName
-
-    model_config = {
-        "from_attributes": True  # from_orm 대신
-    }
 
 app = FastAPI()
 
